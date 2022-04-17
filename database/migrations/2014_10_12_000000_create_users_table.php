28d712088f3139668afc734fa611d777bf166683
o j <90316931+otamajakushi@users.noreply.github.com>
2022-04-17 12:18:59 +0900

    migrationファイル作成

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,20 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->increments('id')->index()->comment('ユーザーID');
+            $table->string('user_status', 20)->index()->default('active')->comment('会員ステータス active or deleted');
+            $table->string('staff', 20)->index()->default('customer')->comment('管理者ステータス customer or staff');
+            $table->string('user_name', 50)->index()->comment('氏名');
+            $table->string('post_code', 7)->index()->comment('郵便番号 数字のみ');
+            $table->string('prefecture', 4)->index()->comment('都道府県');
+            $table->string('municipality', 191)->index()->comment('市区町村番地');
+            $table->string('apartment', 191)->nullable($value = true)->index()->comment('マンション名部屋番号');
+            $table->string('email',191)->unique()->index()->comment('メールアドレス');
+            $table->string('phone_number', 11)->index()->comment('電話番号');
+            $table->date('birthday')->index()->comment('生年月日');
+            $table->string('occupation', 50)->index()->comment('職業');
+            $table->string('gender', 10)->index()->comment('性別 男性or女性orその他');
+            $table->string('password',128)->comment('パスワード');
             $table->timestamps();
         });
     }
