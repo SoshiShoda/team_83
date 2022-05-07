8f2e9ba64b2e123930972a5ffdd017178a523963
otamajakushi <90316931+otamajakushi@users.noreply.github.com>
2022-05-07 13:39:05 +0900

    作業中ページ一覧削除

--- routes/web.php
+++ routes/web.php
@@ -17,64 +17,17 @@ use APP\Http\Controllers;
 
 
 
-/*
+
 Route::get('/', function () {
     return view('welcome');
 });
-*/
 
-/* 
-作業中ページ用ルート一覧
-作業中ページは後日削除します。作業中のページ確認用に作成しました。
+// 在庫管理ページで商品名検索をする
+Route::get('/inventory_management', [App\Http\Controllers\ProductController::class, 'inventory_search'])->name('inventory_searches');
 
-※　新しくルートを作る際の注意
-    　ここより上にルートを書く場合はこの一覧から該当ページを削除してください。
-    　または、この一覧の下に新しくルートを作ってください。
-*/
-// 作業中ページ一覧
-Route::get('/', function () {
-    return view('list_page');
-});
-// 仕入一覧ページ
-Route::get('/purchaseList', function () {
-    return view('admin/purchaseList');
-});
-//仕入登録ページ 
-Route::get('/registerNewPurchase', function () {
-    return view('admin/registerNewPurchase');
-});
-// 販売管理ページ
-Route::get('/salesManagement', function () {
-    return view('admin/salesManagement');
-});
-// 商品編集ページ
-Route::get('/productEdit', function () {
-    return view('admin/productEdit');
-});
-//商品詳細
-Route::get('/productDetail', function () {
-    return view('user/productDetail');
-    });
 // 商品登録ページ
 Route::get('/productRegister', function () {
     return view('admin/productRegister');
-});
-// レビュー編集ページ
-Route::get('/review_edit', function () {
-    return view('user/review_edit');
-});
-// レビュー登録ページ
-Route::get('/review_register', function () {
-    return view('user/review_register');
-});
-/*
-作業用一覧用ルート終了
- */
-
-
-// 在庫管理ページで商品名検索をする
-Route::get('/inventory_management', [App\Http\Controllers\ProductController::class, 'inventory_search']);
-
-
+})->name('productRegister');
 
 
