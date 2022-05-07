cfebe18a0b0baa3ef964ab842b59e3c44f160f71
SoshiShoda <soshi731@gmail.com>
2022-05-07 11:04:57 +0900

    仕入モデル更新

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -14,4 +14,19 @@ class Purchase extends Model
     {
     return $this->belongsTo(Product::class);
     }
+
+    protected $table = 'purchases';
+    public $timestamps = false;
+    protected $fillable = [
+        'id',
+        'product_id',
+        'purchased_price',
+        'purchased_price_with_tax',
+        'purchased_tax_rate',
+        'purchased_quantity',
+        'purchased_date',
+        'arrival_date',
+        'due_date',
+        'payment_date'
+    ];
 }
