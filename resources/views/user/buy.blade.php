49e541ea63c1395c9c295297fc5aff53594f6971
superwata <koga_shiriusu@outlook.jp>
2022-05-25 16:21:59 +0900

    購入完了画面

--- resources/views/user/buy.blade.php
+++ resources/views/user/buy.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/kaiinkanryo.css">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <title>購入</title>
 </head>
@@ -14,27 +14,7 @@
     <!-- ヘッダー部分 -->
     <div class="container">
         <header id="header" class="header">
-            <div class="header-inner">
-                <h1 class="site-title">
-                    <p>demosite</p>
-                </h1>
-
-                <button class="header_hamburger hamburger" id="js-hamburger"> <!--ハンバーガーメニュー-->
-                    <span class="ham_line ham_line1"></span>
-                    <span class="ham_line ham_line2"></span>
-                    <span class="ham_line ham_line3"></span>
-                </button>
-
-                <nav class="header_nav nav" id="js-nav">
-                    <ul class="nav_items nav-items">
-                        <li class="nav-items_item"><a href="">ログイン</a></li>
-                        <li class="nav-items_item"><a href="">マイページ</a></li>
-                        <li class="nav-items_item"><a href="">商品一覧</a></li>
-                        <li class="nav-items_item"><a href="">カート</a></li>
-                        <li class="nav-items_item"><a href="">レビュー</a></li>
-                    </ul>
-                </nav>
-            </div>
+            @include('common.header')
         </header>
 
         <!-- 注文画面 -->
--- resources/views/user/buyConfirmed.blade.php
+++ resources/views/user/buyConfirmed.blade.php
@@ -6,35 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/kaiinkanryo.css">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <title>購入確定</title>
 </head>
 <body>
     <!-- ヘッダー部分 -->
     <div class="container">
-        <header id="header" class="header">
-            <div class="header-inner">
-                <h1 class="site-title">
-                    <p>demosite</p>
-                </h1>
-
-                <button class="header_hamburger hamburger" id="js-hamburger"> <!--ハンバーガーメニュー-->
-                    <span class="ham_line ham_line1"></span>
-                    <span class="ham_line ham_line2"></span>
-                    <span class="ham_line ham_line3"></span>
-                </button>
-
-                <nav class="header_nav nav" id="js-nav">
-                    <ul class="nav_items nav-items">
-                        <li class="nav-items_item"><a href="">ログイン</a></li>
-                        <li class="nav-items_item"><a href="">マイページ</a></li>
-                        <li class="nav-items_item"><a href="">商品一覧</a></li>
-                        <li class="nav-items_item"><a href="">カート</a></li>
-                        <li class="nav-items_item"><a href="">レビュー</a></li>
-                    </ul>
-                </nav>
-            </div>
+    <header id="header" class="header">
+            @include('common.header')
         </header>
 
         <main>
@@ -42,13 +22,15 @@
 
             <input type="button" onclick="" value="商品一覧に戻る">
         </main>
-        
-        <footer class="footer">
-            <div>
-                フッター
-            </div>
-        </footer>
     </div>
+    <script>
+        const ham = document.querySelector('#js-hamburger');
+        const nav = document.querySelector('#js-nav');
+        ham.addEventListener('click', function () {
+            ham.classList.toggle('active');
+            nav.classList.toggle('active');
+        });
+    </script>
 </body>
 </html>
 
