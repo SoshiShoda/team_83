c4fbc3ddedaeb8132c477f36c59f5acccadb0a54
o <90316931+otamajakushi@users.noreply.github.com>
2022-05-29 12:56:50 +0900

    Update buy.blade.php

--- resources/views/user/buy.blade.php
+++ resources/views/user/buy.blade.php
@@ -7,51 +7,81 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
     <title>購入</title>
+    <style>
+        li {
+            list-style: none;
+        }
+    </style>
 </head>
 <body>
-    <!-- ヘッダー部分 -->
-    <header id="header" class="wrapper">
-        @include('common.header')
-    </header>
+    <div class="container">
+        <!-- ヘッダー -->
+        <header id="header" class="header">
+            @include('common.header')
+        </header>
+        <h3>購入画面</h3>
+        <!-- メイン -->
+        <form action="{{ route('buy_fix') }}" method="POST">
+            {{ csrf_field() }}
+            <div id="main">
+                <div id="main-header row">
+                    <p>注文内容の確認</p>
+                    
+                    <p>カートに入っている商品：計{{ $sum_product_quantities->count_id }}点</p>
+                    
+                </div>
+                @foreach ($cart_products as $cart_product)
+                <li>
+                    <div class="product-in-cart bg-light mb-3">
+                        <input type="text" name="user_id" hidden value="{{ $cart_product->user_id }}">
+                        <p>{{ $cart_product->product_name }}</p>
+                        <input type="text" name="product_id" hidden value="{{ $cart_product->product_id }}" >
+                        <div>
+                            <p>税抜価格：{{ $cart_product->bought_price }}円</p>
+                            <input type="text" name="bought_price" hidden value="{{ $cart_product->bought_price }}">
+                        </div>
+                        <div>
+                            <p>税込価格：{{ $cart_product->bought_price_with_tax }}円</p>
+                            <input type="text" name="bought_price_with_tax" hidden value="{{ $cart_product->bought_price_with_tax }}">
+                        </div>
+                        <input type="text" name="bought_tax_rate" hidden value="{{ $cart_product->bought_tax_rate }}"></input>
+                        <div>
+                            <p>数量：<input type="text" name="bought_quantity" value="{{ $cart_product->bought_quantity }}" disabled></input></p>
 
-    <!-- 注文画面 -->
-    <div class="text-center">
-        <div>
-            <div>
-                <div>
-                    <h1>注文内容の確認<span>計点</span></h1>
-                </div>
-                <div>
-                    <!-- 商品名など -->
+                        </div>
+                    </div>
+                </li>
+                @endforeach
+                <div id="total-price">
+                    <p>金額合計：{{ $sum_product_amounts->bought_price }}円</p>
                 </div>
             </div>
-            <div>
-                <h1>品代<span>円</span></h1>
-                <h1>送料<span>円</span></h1>
-                <h1>合計<span>円</span></h1>
+            <div id="user-info" class="">
+                <p id="user-address">お届け先：</p>
+                <p>お名前：{{ $user->user_name }}</p>
+                <p>〒{{ $user->post_code }}</p>
+                <p>ご住所：{{ $user->prefecture }}{{ $user->municipality }}{{ $user->apartment }}</p>
+
+                <p id="pay-method">お支払方法：</p>
+                <select name="payment_method" class="form-control">
+                    <option value="クレジットカード" selected="selected">クレジットカード</option>
+                    <option value="銀行振込">銀行振込</option>
+                    <option value="コンビニ/郵便局での現金">コンビニ/郵便局での現金</option>
+                </select>
             </div>
-        </div>
-        <div>
             <div>
-                <h1>お客様情報</h1>
+                <button type="submit" class="form-control btn btn-primary">注文を確定する</button>
             </div>
-            <div>
-                <div>
-                    <h1>お届け先</h1>
-                </div>
-                <div>
-                    <h1>お支払い方法</h1>
-                </div>
-            </div>
-        </div>
-        <!-- 注文確定ボタン -->
-        <div>
-            <input type="button" onclick="" value="注文を確定する">
-        </div>
+        </form>
     </div>
-    <script src="{{ asset('/js/header.js') }}"></script>
+    <script>
+        const ham = document.querySelector('#js-hamburger');
+        const nav = document.querySelector('#js-nav');
+        ham.addEventListener('click', function () {
+            ham.classList.toggle('active');
+            nav.classList.toggle('active');
+        });
+    </script>
 </body>
 </html>
-
